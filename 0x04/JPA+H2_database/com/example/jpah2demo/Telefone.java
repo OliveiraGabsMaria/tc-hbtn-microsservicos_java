440e4c56a54720870542b78e09c763dce6d193b2
Gabrielly Oliveira <gabriellyorosimbooliveira@gmail.com>
2022-06-27 19:23:01 -0300

    Imprimir task 0

---
+++ 0x04/JPA+H2_database/com/example/jpah2demo/Telefone.java
@@ -0,0 +1,69 @@
+package com.example.jpah2demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+@Entity
+@Table(name = "telefone")
+public class Telefone {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String ddd;
+	
+	@Column
+	private String numero;
+	
+	@ManyToOne
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	private Cliente cliente;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDdd() {
+		return ddd;
+	}
+
+	public void setDdd(String ddd) {
+		this.ddd = ddd;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return this.id + " - " + this.ddd + " - " + this.numero;
+	}
+}
